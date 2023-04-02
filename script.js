e4266a3807c4af04b21ca333ea0f80d7d2d4c448
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-04-02 23:32:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+// Get all the price elements from the table
+const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+// Initialize a variable to store the total price
+let totalPrice = 0;
+
+// Loop through each price element and add its value to the total price
+priceElements.forEach((element) => {
+  totalPrice += Number(element.textContent);
+});
+
+// Set the total price in the table
+const totalElement = document.querySelector('[data-ns-test="grandTotal"]');
+totalElement.textContent = totalPrice;
